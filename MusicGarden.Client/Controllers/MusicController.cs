0f3662b78ee9f3ab2525873d8ced4d1c8747fa4b
marcangy cange <marcangy.cange@revature.net>
2021-05-21 21:34:56 -0400

    Connected to API

--- MusicGarden.Client/Controllers/MusicController.cs
+++ MusicGarden.Client/Controllers/MusicController.cs
@@ -1,9 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
 
 namespace Medialab.Client.Controllers
 {
@@ -11,20 +17,43 @@ namespace Medialab.Client.Controllers
   [Route("[controller]")]
   public class MusicController : ControllerBase
   {
-
-
+    private static readonly HttpClient client = new HttpClient();
+    private IConfiguration _configuration;
+    public MusicController(IConfiguration configuration)
+    {
+      _configuration = configuration;
+    }
 
     [HttpGet]
-    public List<string> Get()
+    public object Index()
     {
+      var response = client.GetAsync($"{_configuration["Services:webapi"]}").GetAwaiter().GetResult();
 
-      return new List<string>{
 
-                "R&B",
-                "Rap"
 
-            };
+      if (response.IsSuccessStatusCode)
+      {
+        var result = JsonConvert.DeserializeObject(response.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+
+        return result;
+      }
+
+      return null;
 
     }
+
+
+    // [HttpGet]
+    // public List<string> Get()
+    // {
+
+    //   return new List<string>{
+
+    //             "R&B",
+    //             "Rap"
+
+    //         };
+
+    //}
   }
 }
